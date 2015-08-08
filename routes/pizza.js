3787cca39c8e3b7d7f0398120b69918d4a2bd11d
calebgregory <calebgregory@gmail.com>
2015-08-07 22:53:29 -0500

    Changing pizza shop to use mongodb

--- routes/pizza.js
+++ routes/pizza.js
@@ -1,13 +1,57 @@
 var express = require('express');
+var moment = require('moment');
+var ObjectID = require('mongodb').ObjectID;
+
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  var obj = {
-    qty     : req.query.qty || 1,
-    topping : req.query.topping || 'cheese',
-    title   : 'my\'a pizza pie-ya shop\'a'
+
+  var collection = global.db.collection('pizzas');
+  collection.find({}).toArray(function(err,orders){
+    var formattedOrders = orders.map(function(order) {
+      return {
+        _id       : order._id,
+        name      : order.name,
+        toppings  : order.toppings.join(', '),
+        complete  : order.complete,
+        createdAt : moment(order._id.getTimestamp()).fromNow()
+      };
+    });
+    res.render('templates/pizza-index',
+              { orders : formattedOrders });
+  });
+
+});
+
+router.get('/order', function(req,res) {
+  res.render('templates/pizza-new');
+});
+
+router.post('/order', function(req,res) {
+
+  var collection = global.db.collection('pizzas');
+  var newOrder = {
+    name      : req.body.name,
+    toppings  : req.body.toppings,
+    complete  : false
   };
-  res.render('templates/pizza', obj);
+  collection.save(newOrder, function() {
+    res.redirect('/pizza');
+  });
+
+});
+
+router.post('/order/:id/complete', function(req,res) {
+
+  var collection = global.db.collection('pizzas');
+  collection.update(
+    { _id : ObjectID(req.params.id) },
+    { $set : {complete:true} },
+    function() {
+      res.redirect('/pizza');
+    }
+  );
+
 });
 
 module.exports = router;
