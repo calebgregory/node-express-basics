405d20afdf2696782e478f3ab620e8364dadbe8b
calebgregory <calebgregory@gmail.com>
2015-08-10 10:00:18 -0500

    Check heroku env variables

--- app.js
+++ app.js
@@ -129,5 +129,6 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
+  console.log(process.env);
   console.log('Example app listening at http://%s:%s', host, port);
 });
